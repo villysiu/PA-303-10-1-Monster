f4ca2c6a2746e84c202ce7648db6cc06bacb66b1
Villy Siu <villysiu@gmail.com>
2024-12-05 15:03:01 -0800

    Initial commit

---
+++ src/Monster.java
@@ -0,0 +1,9 @@
+public class Monster {
+    private String name;
+    public Monster(String name) {
+        this.name = name;
+    }
+    public String attack(){
+        return "!^_&^$@+%$* I don't know how to attack!\n";
+    }
+}
